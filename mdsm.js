15ae0d1ce92bd3a39004e4bf4da28093b94e5ab0
Juan Jauregui <juan@jauregui.co>
2017-11-16 21:43:04 -0500

    Added HTTPS configuration options and documentation

--- mdsm.js
+++ mdsm.js
@@ -1,25 +1,50 @@
 const Client = require("./Classes/Client.js");
-const Client = require("./Classes/ClientClass.js");
-const Client = require("./Classes/Session.js");
+const ClientClass = require("./Classes/ClientClass.js");
+const Session = require("./Classes/Session.js");
 
 let mdsm = function(){
-	var port;
+	var port;		// Server will listen to requests to this port
+	var rootUrl;	// The root URL for all endpoint calls (e.g. '/api/')
 
-	let sessions = [];
-	let endpoints = [];
-	let clientClasses = [];
+	/* HTTPS is optional. */
+	var httpsConfig = {
+		httpsEnabled: false,
+		key: null,
+		cert: null,
+		ca: null
+	};
 
+	let sessions = [];			// Will be populated with sessions as they are initialized
+	let endpoints = [];			// Will contain a list of valid endpoints
+	let clientClasses = [];		// Will contain a list of valid client classes
+
+	/* Initializes an instance based on the given configuration. See documentation for "config" schema */
 	let init = function(config){
 		port = config.port;
 		url = config.url;
+		if(config.https){
+			httpsConfig.httpsEnabled = true;
+			httpsConfig.key = config.https.key;
+			httpsConfig.cert = config.https.cert;
+			httpsConfig.ca = config.https.ca;
+		}
+	}
+
+	let createClientClass = function(clientClassID){
+		let newClientClass = new ClientClass(clientClassID);
+		return newClientClass;
+	}
+
+	/* Create a new endpoint */
+	let addEndpoint = function(newEndpointInfo){
+		endpoints.insert(newEndpointInfo.url);
+		if(!(clientClasses.includes(newEndpointInfo.allowedClassTypes))){
+			clientClasses.insert(newEndpointInfo.allowedClassTypes);
+		}
 	}
 
 	let processRequest = function(req){
 
-	}
-
-	let createClientClass = function(clientClassID){
-		clientClasses.insert(new ClientClass(clientClassID));
 	}
 
 	/* Allows the framework to be used as Express.js middleware. Usage:
@@ -35,6 +60,7 @@ let mdsm = function(){
 	return {
 		init: init,
 		createClientClass: createClientClass,
+		addEndpoint: addEndpoint,
 		processRequest: processRequest,
 		expressHook: expressHook
 	}
--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 const mdsm = require('./mdsm')
 
 mdsm.init({
-	port: 9001,
-	url: '/api/'
+	port: 9001,				// Listen on port 9001
+	url: '/api/',			// All endpoints will be prefixed with /api/
+	https: false			// Don't use HTTPS
 });
 
 let endpointURL = 'doSomething1';
-let clientClassA = 
-let allowedClassTypes = [];
+let allowedClassTypes = [
+	mdsm.createClientClass('display');
+];
 mdsm.addEndpoint({
 	url: endpointURL,
-
+	allowedClassTypes: allowedClassTypes
 });
